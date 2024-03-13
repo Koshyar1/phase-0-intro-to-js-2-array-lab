7223d473e3738b667b81634c52594f76eca0737d
koshyar <ensw03420@student.su.edu.krd>
2024-03-13 13:27:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+
+function appendCat(name) {
+  let catList = [...cats];
+  catList.push(name);
+  return catList;
+}
+
+console.log(appendCat("rawa"));
+console.log(cats);
+
+function prependCat(name) {
+  let catList = [...cats];
+  catList.unshift(name);
+  return catList;
+}
+
+function removeFirstCat(name) {
+  let catList = [...cats];
+  catList.shift(name);
+  return catList;
+}
+
+function removeLastCat(name) {
+  let catList = [...cats];
+  catList.pop(name);
+  return catList;
+}
